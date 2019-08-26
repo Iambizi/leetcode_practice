ea345e7e06f2f70bf1d8b207f6b787ff2074bf0f
Amir Bizimana <amirbzmn@gmail.com>
2019-08-25 23:13:27 -0400

    comments explaining LC20

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -1,8 +1,13 @@
+
+// we solve this problem using a stack
 const isValid = s => {
     let stack = [];
     let pairsHashMap = { "(": ")", "{": "}", "[": "]" };
-
+    // we are going to iterate through every character in our input string
+    // during for loop if char we are on is one the left ones we append to stack
+    // if char we are on is on the right (the closing one of the pairs or inverse) we pop it off the stack
     for (let i = 0; i < s.length; i++) {
+        // current char we are on
         let char = s[i];
 
         if (pairsHashMap[char]) {
@@ -11,6 +16,7 @@ const isValid = s => {
             return false;
         }
     }
+    // we need to check that stack length is indeed = 0 before we return true;
     return stack.length === 0;
 };
 
