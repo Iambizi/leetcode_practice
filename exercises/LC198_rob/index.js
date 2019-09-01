8da6c0b765734a63cc92ae13ee6d12dfd7d68f37
Amir Bizimana <amirbzmn@gmail.com>
2019-08-31 21:50:06 -0400

    LC300 Length of increasing sequence explained and solved

--- exercises/LC198_rob/index.js
+++ exercises/LC198_rob/index.js
@@ -4,9 +4,9 @@ function rob(nums) {
     if (nums.length === 0) return 0;
     if (nums.length === 1) return nums[0];
     if (nums.length === 2) return Math.max(nums[0], nums[1]);
-
+    // max loot to rob for a 1 or 2 house scenario
     let maxLootAtNth = [nums[0], Math.max(nums[0], nums[1])]
-
+    // max loot for 3rd or more future houses to rob
     for (let i = 2; i < nums.length; i++) {
         maxLootAtNth.push(Math.max(nums[i] + maxLootAtNth[i - 2], maxLootAtNth[i - 1]));
     }
