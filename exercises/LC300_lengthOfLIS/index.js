250df4fb362db3a6c4b79a0843e89dd5fcb0ecd8
Amir Bizimana <amirbzmn@gmail.com>
2019-09-01 12:31:41 -0400

    LC300 update

--- exercises/LC300_lengthOfLIS/index.js
+++ exercises/LC300_lengthOfLIS/index.js
@@ -1,10 +1,12 @@
+// we will use a dp array to help solve this problem
 function lengthOfLIS(nums) {
     // edge case cause if no number in sequence then biggest increase is indeed 0
     if (nums.length === 0) return 0;
-
+    // our dp array is the same length as our input array and initially filled with ones
     let dpSubsequence = new Array(nums.length).fill(1);
     let maxSoFar = 1;
 
+    // j and i markers that loop through our input array
     for (let j = 1; j < nums.length; j++) {
         for (let i = 0; i < j; i++) {
             if (nums[j] > nums[i]) {
