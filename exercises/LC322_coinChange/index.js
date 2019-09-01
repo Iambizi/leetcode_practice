22744d3efa9c4d2e157cb05ff3f64361ba42d67d
Amir Bizimana <amirbzmn@gmail.com>
2019-09-01 12:33:15 -0400

    LC322 coinChange explained and solved

--- exercises/LC322_coinChange/index.js
+++ exercises/LC322_coinChange/index.js
@@ -1,3 +1,18 @@
-function coinChange(coins, amount) {}
+function coinChange(coins, amount) {
+    let dpMinCoins = new Array(amount + 1).fill(Infinity);
+    dpMinCoins[0] = 0;
+
+    //nested for loops fill up our dp array 
+    for (let i = 1; i < dpMinCoins.length; i++) {
+        for (let j = 0; j < coins.length; j++) {
+            const coinValue = coins[j];
+            if (coinValue <= i) {
+                dpMinCoins[i] = Math.min(dpMinCoins[i - coinValue] + 1, dpMinCoins[i])
+            }
+        }
+    }
+    const answer = dpMinCoins[dpMinCoins.length - 1];
+    return answer === Infinity ? -1 : answer;
+}
 
 module.exports = coinChange;
