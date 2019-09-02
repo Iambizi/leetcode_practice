3700829687a442964c28069ec43659fd36204353
Amir Bizimana <amirbzmn@gmail.com>
2019-09-01 23:34:15 -0400

    LC252 canAttendMeetings explained and solved

--- exercises/LC252_canAttendMeetings/index.js
+++ exercises/LC252_canAttendMeetings/index.js
@@ -4,6 +4,31 @@
 // canAttendMeetings([[0, 30], [5, 10], [15, 20]]) --> false
 // canAttendMeetings([[7, 10], [2, 4]]) --> true
 
-const canAttendMeetings = intervals => {};
+const canAttendMeetings = intervals => {
+    // separate start and end times in 2 distinctive arrays and sort them from lease to greates
+    // we loop through Starts array
+    /* check if the start time at one index ahead is less than end time at current index 
+    
+    If so it overlaps and you can't attend meeting*/
+    //hence return false
+    const starts = [];
+    const ends = [];
+
+    //Fill up starts and ends
+    for (let i = 0; i < intervals.length; i++) {
+        const subArray = intervals[i];
+        starts.push(subArray[0]);
+        ends.push(subArray[1]);
+    }
+    // sorting both array from least to greatest
+    starts.sort((a, b) => a - b);
+    ends.sort((a, b) => a - b);
+
+    for (let i = 0; i < starts.length; i++) {
+        if (starts[i + 1] < ends[i]) return false;
+    }
+    // when the loop finishes finishes and never returns false then we return true
+    return true;
+};
 
 module.exports = canAttendMeetings;
