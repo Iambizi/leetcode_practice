daa7dec98bf6036cf553776c045a6ff629947626
Amir Bizimana <amirbzmn@gmail.com>
2019-09-07 15:33:11 -0400

    LC104 maxDepth explained and solved

--- exercises/LC104_maxDepth/index.js
+++ exercises/LC104_maxDepth/index.js
@@ -1,3 +1,28 @@
-function maxDepth(root) {}
+// we solve this using helper method recursion
+function maxDepth(root) {
+    let maxDepth = 0;
+
+    //our helper function
+    function dive(node, currentDepth){
+        // base case to avoid infinite recursion
+        /*
+            if we reach this scenario were some kind of depth has been reached 
+            cause we exhausted our tree, then we need to update our maxDepth var
+        */
+        if(!node){
+            // set to previously set maxDepth or currentDepth -1
+            maxDepth = Math.max(currentDepth -1, maxDepth );
+            return;
+        }
+        dive(node.left,currentDepth + 1)
+        dive(node.right,currentDepth + 1)
+    }
+    /* 
+        once we call our helper function and it does its recursions, 
+        maxDepth will be updated correctly 
+    */
+    dive(root, 1);
+    return maxDepth;
+}
 
 module.exports = maxDepth;
