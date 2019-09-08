1fab1c9d52f199b646b52340c74c57fceb0f3fda
Amir Bizimana <amirbzmn@gmail.com>
2019-09-07 22:43:13 -0400

    sudo code

--- exercises/LC235_lowestCommonAncestor/index.js
+++ exercises/LC235_lowestCommonAncestor/index.js
@@ -1,3 +1,8 @@
-function lowestCommonAncestor(root, p, q) {}
+function lowestCommonAncestor(root, p, q) {
+    //Start at Root
+    //If Root val < than both Input Node Vals move right
+    //If Root val > than both Input Node vals move left
+    //If Root val is neither, we are the Lowest Common Ancestor
+}
 
 module.exports = lowestCommonAncestor;
