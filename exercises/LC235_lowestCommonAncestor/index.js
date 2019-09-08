97f9c0e38153524539f471adde6c031ce6012b28
Amir Bizimana <amirbzmn@gmail.com>
2019-09-07 22:52:48 -0400

    LC235_LowestCommonAncestor solved

--- exercises/LC235_lowestCommonAncestor/index.js
+++ exercises/LC235_lowestCommonAncestor/index.js
@@ -3,6 +3,21 @@ function lowestCommonAncestor(root, p, q) {
     //If Root val < than both Input Node Vals move right
     //If Root val > than both Input Node vals move left
     //If Root val is neither, we are the Lowest Common Ancestor
+    const pVal = p.val;
+    const qVal = q.val;
+
+    let currentNode = root;
+
+    while(currentNode){
+        const currentVal = currentNode.val;
+        if(currentVal < pVal && currentVal < qVal){
+            currentNode = currentNode.right;
+        }else if(currentVal > pVal && currentVal > qVal){
+            currentNode = currentNode.left;
+        }else {
+            return currentNode;
+        }
+    }
 }
 
 module.exports = lowestCommonAncestor;
