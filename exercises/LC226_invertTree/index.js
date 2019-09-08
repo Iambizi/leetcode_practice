fc0b6a4768dad894c28d4a7da8bba1bd84398c4a
Amir Bizimana <amirbzmn@gmail.com>
2019-09-07 21:56:36 -0400

    LC226_invertTree explained and solved

--- exercises/LC226_invertTree/index.js
+++ exercises/LC226_invertTree/index.js
@@ -1,3 +1,18 @@
-function invertTree(root) {}
+//we solve this with helper method recursion
+function invertTree(root) {
+    // helper function that helps with inverting all children nodes
+    function helper(node){
+        if(!node) return;
+        // create temp var to store old value of node.left
+        const temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        
+        helper(node.left);
+        helper(node.right);
+    }
+    helper(root);
+    return root;
+}
 
 module.exports = invertTree;
